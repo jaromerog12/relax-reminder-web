c8c576a38cdab8693d0d0ea3ff0279cb849b9148
Jorge A. Romero <ja.romerog12@uniandes.edu.co>
2024-03-03 23:32:46 -0500

    rutas de login y dashboard ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {DashboardComponent} from "./dashboard/dashboard/dashboard.component";
+import {HomeComponent} from "./dashboard/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full'},
+  { path: 'dashboard',
+  component: DashboardComponent,
+  children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+  ]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
