ef1b29196cfb71baa392be1ef690be6746071673
Kevin Alexander Maldonado Delgado <k.maldonadod@uniandes.edu.co>
2024-03-07 00:56:07 -0500

    ajuste redireccion menu

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {SidebarComponent} from "./sidebar/sidebar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterLink } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [SidebarComponent, NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   exports: [SidebarComponent, NavbarComponent]
 })
