3a46d6f2f7d0f5946072c78c3658fe9e1f30f868
Jorge A. Romero <ja.romerog12@uniandes.edu.co>
2024-03-07 00:08:57 -0500

    create activity ok

---
+++ src/app/dashboard/activity/create-activity/create-activity.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit} from '@angular/core';
+import {TitleService} from "../../../utils/TitleService";
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-create-activity',
+  templateUrl: './create-activity.component.html',
+  styleUrls: ['./create-activity.component.css']
+})
+export class CreateActivityComponent implements OnInit{
+  nameRemainingChars: number = 20
+  nameMaximumChars: number = 20;
+  descriptionRemainingChars: number = 300
+  descriptionMaximumChars: number = 300;
+  constructor(private titleService: TitleService) {
+  }
+
+  ngOnInit() {
+    this.sendDataToDashboard();
+  }
+
+  sendDataToDashboard(){
+    const dataToSend = {
+      title: "Crear actividad"
+    }
+    this.titleService.setData(dataToSend);
+  }
+
+  nameUpdateCharCount(event: any): void {
+    const value = (event.target as HTMLInputElement).value;
+    if (value.length > this.nameMaximumChars) {
+      (event.target as HTMLInputElement).value = value.slice(0, this.nameMaximumChars);
+    }
+    this.nameRemainingChars = this.nameMaximumChars - (event.target as HTMLInputElement).value.length;
+  }
+
+  descriptionUpdateCharCount(event: any): void {
+    const value = (event.target as HTMLInputElement).value;
+    if (value.length > this.descriptionMaximumChars) {
+      (event.target as HTMLInputElement).value = value.slice(0, this.descriptionMaximumChars);
+    }
+    this.descriptionRemainingChars = this.descriptionMaximumChars - (event.target as HTMLInputElement).value.length;
+  }
+
+  save() {
+    Swal.fire({
+      title: "<strong>Buen trabajo!!!</strong>",
+      icon: "success",
+      html: `
+    Actividad creada satisfactoriamente
+  `,
+      showCloseButton: true,
+      showCancelButton: false,
+      focusConfirm: false,
+      confirmButtonText: `
+    Aceptar
+  `,
+      confirmButtonAriaLabel: "Thumbs up, great!",
+      customClass: {
+        confirmButton: 'button-primary'
+      }
+    });
+  }
+}
