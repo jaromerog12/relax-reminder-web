15242252b53a9d2b945d8753367717c56bfd843e
Jorge A. Romero <ja.romerog12@uniandes.edu.co>
2024-03-07 23:06:05 -0500

    Ajuste del boton cerrar sesion

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,16 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from "./sidebar/sidebar.component";
 import { NavbarComponent } from './navbar/navbar.component';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 
 
 @NgModule({
   declarations: [SidebarComponent, NavbarComponent],
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
+    imports: [
+        CommonModule,
+        RouterLink,
+        RouterLinkActive
+    ],
   exports: [SidebarComponent, NavbarComponent]
 })
 export class MenuModule { }
